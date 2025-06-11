e31daa10b55f3d803dd0917ff642b80c1a1c65ee
nghfp9wa7bzq <nghfp9wa7bzq@gmail.com>
2025-06-11 19:20:01 +0200

    Add license to files

--- tusb_config.h
+++ tusb_config.h
@@ -2,6 +2,7 @@
  * The MIT License (MIT)
  *
  * Copyright (c) 2019 Ha Thach (tinyusb.org)
+ * Copyright (c) 2020-2025 Patrick Dussud
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
